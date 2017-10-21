b9585cd72e70f9986a4c28dfca9b5db3e8ed2201
priscilla <priscillacosta@id.uff.br>
2017-10-21 15:40:04 +0200

    image field

--- main_app/urls.py
+++ main_app/urls.py
@@ -14,8 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf import settings
+from django.views.static import serve
 from . import views
 
+
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.index, name = 'index'),
+    url(r'^([0-9]+)/$' ,
+    	views.detail, name = 'detail'),
+    url(r'^post_url/$' ,
+        views.post_treasure, name = 'post_treasure'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
--- treasuregram/settings.py
+++ treasuregram/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
