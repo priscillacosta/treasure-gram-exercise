b01b982db15070ab4042ec6620bb994b8edd7413
priscilla <priscillacosta@id.uff.br>
2017-10-29 15:06:35 +0100

    login

--- main_app/urls.py
+++ main_app/urls.py
@@ -20,11 +20,15 @@ from . import views
 
 
 urlpatterns = [
+    url(r'^user/(\w+)/$', views.profile, name = 'profile'),
     url(r'^$', views.index, name = 'index'),
     url(r'^([0-9]+)/$' ,
     	views.detail, name = 'detail'),
     url(r'^post_url/$' ,
         views.post_treasure, name = 'post_treasure'),
+    url(r'^login/$', views.login_view, name='login'),
+    url(r'^logout/$', views.logout_view, name='logout'),
+
 ]
 
 if settings.DEBUG:
